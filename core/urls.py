d18223ce7082d17aca6ac5be5ccd86b51a74472a
pedrolisboaa <phlisboa2000@gmail.com>
2023-10-03 23:20:27 -0300

    finalizando de forma simples login, cadastro e acesso a plataforma

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index, login, cadastro
+from .views import index, login, cadastro, plataforma
 
 
 urlpatterns = [
     path('', index, name='index'),
     path('login/', login, name='login'),
-    path('cadastro/',cadastro, name='cadastro' )
+    path('cadastro/',cadastro, name='cadastro' ),
+    path('plataforma/', plataforma, name='plataforma'),
 ]
 
--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as login_django
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -14,20 +16,30 @@ def cadastro(request):
     if request.method == 'GET':
         return render(request, 'cadastro.html')
     else:
-        nome = request.POST.get('nome') 
+        login = request.POST.get('login') 
         email = request.POST.get('email')
         senha = request.POST.get('senha')
         confirmacao_senha = request.POST.get('senha1')
 
-        usuario = User.objects.filter(email=email).first()
+        usuario = User.objects.filter(username=login).first()
+        email = User.objects.filter(email=email).first()
 
-        if usuario:
-            print('Já existe um usuário com esse login.')
+        # Protegendo meu login
+        if not login.strip():
+            messages.error(request, 'Login não pode estar em branco.')
             return render(request, 'cadastro.html')
 
-        usuario = User.objects.create_user(username=nome, email=email, password=senha)
+        if senha != confirmacao_senha: 
+            messages.error(request, 'As senhas são diferentes.')
+            return render(request, 'cadastro.html')
+
+        if usuario is not None or email is not None:
+            messages.error(request, 'Já existe um usuário com esse login ou email.')
+            return render(request, 'cadastro.html')
+
+        usuario = User.objects.create_user(username=login, email=email, password=senha)
         usuario.save()
-        print('Usuário cadastro com sucesso.')
+        messages.success(request, 'Usuário cadastro com sucesso.')
         return render(request, 'cadastro.html')
 
 
@@ -36,13 +48,20 @@ def login(request):
     if request.method == 'GET':
         return render(request, 'login.html')
     else:
-        email = request.POST.get('email')
+        
+        login = request.POST.get('login')
         senha = request.POST.get('senha')
 
-        usuario = authenticate(email=email, password=senha)
+        usuario = authenticate(username=login, password=senha)
 
         if usuario:
             login_django(request, usuario)
-            return HttpResponse('Autenticado')
+            return redirect(plataforma)
         else:
-            return HttpResponse('Email ou senha inválidos')
+            messages.error(request, 'Login ou Senha não conferem.')
+            return render(request, 'login.html')
+
+
+@login_required(login_url='login')
+def plataforma(request):
+    return render(request, 'plataforma.html')
